7479a9a599bacf24f6a3d369701ab646b0528903
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-11 15:01:06 +0100

    index.erb file

--- lib/app.rb
+++ lib/app.rb
@@ -1,31 +1,5 @@
 require 'sinatra'
 
-get '/' do
-  "hello!"
-end
-
-get '/secret' do
-  "meow"
-end
-
-get '/lyla' do
-  "lyla is a cat"
-end
-
-get '/makers' do
-  "makers will change our lives"
-end
-
-get '/banana' do
-  'BANANA!!'
-end
-
-get '/lucian' do
-  'knows about the web!'
-end
-
 get '/cat' do
-  "<div style ='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb(:index)
 end
